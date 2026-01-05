243fc6c4888181d3ecdff6fa95adc9a7b10dfc3c
Zia Abdurrofi <ziaabdurrofi@gmail.com>
2026-01-05 10:38:48 +0700

    Add Tailwind CDN fallback for production

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,6 +13,7 @@
         rel="stylesheet">
 
     <!-- Scripts -->
+    <script src="https://cdn.tailwindcss.com"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
         body {
